8f0537c66bc8c3d5c3e797036b98b43ccc4ce05a
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2022-04-04 13:03:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+array_1=["pen","paper","book","bottle"];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+console.log(Element_of_array);
+
+
+timer_counter = "";
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = "";
